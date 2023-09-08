00a6d6410452b28c6b7967cee07c7aa4fe89ea22
vennila205 <144460111+vennila205@users.noreply.github.com>
2023-09-08 20:01:18 +0530

    Add files via upload

---
+++ unit1/main.py
@@ -0,0 +1,27 @@
+# 1.1 lmplement  recursive function to calculate the factorial of a given number 
+
+"""
+1! = 1 × 1
+2! = 2 × 1!
+3! = 3 × 2!
+.
+.
+10! = 10 × 9!
+
+formula - n × (n-1):
+"""
+
+
+from typing import MutableMapping
+
+
+def fact_rec(n):
+ if n==0 or n==1:
+   return 1
+ else:
+   return n*fact_rec(n-1)
+
+number = int(input("Enter a number :"))
+res = fact_rec(number)
+
+print("The factorial of {} is {}.".format(number,res))
